b8511a71dcc88ea24ed854231a5f96e58351f646
santosh ghimire <santoshghimire55@outlook.com>
2022-09-20 16:16:00 +0545

    updated code

--- src/booking/models.py
+++ src/booking/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from src.rooms.models import Room
 
-
-
 class Booking(models.Model):
     room= models.ForeignKey(Room, on_delete=models.PROTECT)
     price= models.DecimalField(max_digits=10,decimal_places=2)
